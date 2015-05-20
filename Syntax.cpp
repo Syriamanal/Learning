5d784c87b5a0f7e4b04e77c9ae13138b7344a215
Syriamanal <shadinachat123@gmail.com>
2015-05-20 08:13:43 -0700

    Update Syntax.cpp

--- Syntax.cpp
+++ Syntax.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
-int main()
-{
-
+int main(){
+    string mstr = ("2165165");
+    int myint;
+    stringstream(mstr) >> myint;
+    cout << myint << endl;
 }
